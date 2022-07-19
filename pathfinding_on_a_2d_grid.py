366b63d82b0822f475e1cb6d97e1430e8ccba06a
ChiaraDM <demattec@roehampton.ac.uk>
2022-07-19 19:43:57 +0100

    Phase 2 solved + refactorisation

--- pathfinding_on_a_2d_grid.py
+++ pathfinding_on_a_2d_grid.py
@@ -1,3 +1,5 @@
+import random
+
 # PHASE 1
 
 # Implement a 10x10 grid that contains a starting point
@@ -7,7 +9,10 @@
 # Your algorithm should calculate a valid path avoiding 
 # the obstacles and reaching the delivery point.
 
-def make_grid():
+# Your solution should print the path in the format of 
+# [(x1, y1), (x2, y2)... . .] and also the number of steps.
+
+def create_empty_grid():
     """Make a grid 10x10."""
     grid = []
     rows, cols = (10,10)
@@ -19,18 +24,20 @@ def make_grid():
     
     return grid
 
-def formatting(steps=None):
+def print_grid(grid, steps=None):
+    """Print the grid formatted with a list on each line."""
     visual_grid = grid[:]
     if not steps is None:
         for step in steps[:-1]:
             visual_grid[step[0]][step[1]] = ">"
+
     print('\n'.join([str(lst) for lst in visual_grid]))
 
-def is_valid(coordinates, grid):
+def can_step_to_coordinate(coordinates, grid):
     """Check if a location is valid.
     Is it in the boundaries? Is it an obstacle?"""  
     x, y = coordinates 
-    if 0 <= x < len(grid) and 0 <= y < len(grid[x]):
+    if 0 <= x and x < len(grid) and 0 <= y and y < len(grid[x]):
         if grid[x][y] == 1:
             return False
         else:
@@ -38,28 +45,33 @@ def is_valid(coordinates, grid):
     else:
         return False
 
-def find(element, matrix):
-    """Returns a tuple of coordinates.
+def find_value_in_grid(element, grid):
+    """Return a tuple of coordinates.
     Not really useful in this case as the matrix
     contains multiple zeros so it can only track 
     the first occurrance."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == element:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == element:
                 return (i, j)
 
-def get_adjacent(coordinates, grid):
-    """Checks adjacent values to the selected cell.
+def get_valid_adjacent_coordinates(coordinates, grid):
+    """Check adjacent values to the selected cell.
     Returns a list of adjacent coordinates."""
     x, y = coordinates
-    l = []
+    steps = []
 
-    for check_one in [-1, 0, 1]:
-        for check_two in [-1, 0, 1]:
-            position = (x+check_one, y+check_two)
-            if not (check_one == check_two == 0) and is_valid(position, grid):
-                l.append(position) 
-    return l
+    for x_change in [-1, 0, 1]:
+        for y_change in [-1, 0, 1]:
+            if x_change == 0 and y_change == 0:
+                continue
+
+            position = (x+x_change, y+y_change)
+
+            if can_step_to_coordinate(position, grid):
+                steps.append(position)
+
+    return steps
 
 def euclidian_distance(point_one, point_two):
     """Calculate the distance between two coordinates."""
@@ -76,54 +88,94 @@ def find_path(starting_point, ending_point):
     current_distance = euclidian_distance(starting_point, ending_point)
 
     while current_position != ending_point:
-        list_of_possible_steps = get_adjacent(current_position, grid)
+        list_of_adjacent_coordinates = get_valid_adjacent_coordinates(current_position, grid)
+        if len(list_of_adjacent_coordinates) == 0:
+            print("There are no valid steps from location", current_position, "!\n")
+            break
 
-        for step in list_of_possible_steps:
-            distance = euclidian_distance(step, ending_point)
+        we_are_moving_away = True
+        for coordinate in list_of_adjacent_coordinates:
+            distance = euclidian_distance(coordinate, ending_point)
 
-            if current_distance > distance:
+            if distance < current_distance:
+                
                 current_distance = distance
-                current_position = step
-                # Testing better cell
-                #print("Found better cell:", current_distance, current_position)
-        
-        # For testing
-        #print("Stepping to:", current_position)
+                current_position = coordinate
+                we_are_moving_away = False
+                
+        if we_are_moving_away:
+            print("There are no valid steps from location", current_position, "!\n")
+            break
+
         path.append(current_position)
-        
                 
         if path[-1] == ending_point:
-            #print("We reached the final destination: ", current_position)
             break
         
-    return path       
+    return path
         
-#print("This is the current path: ",find_path(starting_point, ending_point))
+# grid
+grid = create_empty_grid()
 
-grid = make_grid()
-
-# starting point
+# starting, ending points
 grid[0][0]="s"
-
-# ending point
 grid[9][9]="e"
 
 # obstacles locations
-grid[9][7]=1
-grid[8][7]=1
-grid[6][7]=1
-grid[6][8]=1
+obstacles_location = [(9,7), (8,7), (6,7), (6,8)]
+for obstacle_loc in obstacles_location:
+    x = obstacle_loc[0]
+    y = obstacle_loc[1]
+    grid[x][y] = 1
 
-print("This is the original grid: \n s = starting point \n e = ending point \n 0 = accessible cell \n 1 = obstable")
-formatting()
-
-starting_point = find("s", grid)
-ending_point = find("e", grid)
-
+starting_point = find_value_in_grid("s", grid)
+ending_point = find_value_in_grid("e", grid)
 path = find_path(starting_point, ending_point)
 
+# initial grid
+
+print("PHASE 1\n")
+print("This is the original grid: \n s = starting point \n e = ending point \n 0 = accessible cell \n 1 = obstable \n > = visual path \n")
+print_grid(grid)
+#print("\n> for visual path: \n")
+#print_grid(grid, path)
+print("\nPATH Phase 1: ", path)
+print("Number of steps: ", len(path), "\n")
+
+# PHASE 2
+
+# Add an additional 20 randomly placed obstacles and print their location using the format [(x1, y2), (x2, y2)... . .]
+# The obstacles should not overlap existing ones and should not be placed at the start and delivery points.
+# Your algorithm should calculate a valid path avoiding the obstacles and reaching the delivery point.
+# Your solution should print the path in the format of [(x1, y2), (x2, y2)... . .]
+
+def random_obstacles(grid):
+    """Place 20 random obstacles and 
+    return a list of their coordinates."""
+    random_obstacles_loc = []
+    current_obstacle = 0
+    while current_obstacle < 30:
+        x = random.randrange(10)
+        y = random.randrange(10)
+            
+        if (x,y) not in obstacles_location and (x,y) != starting_point and (x,y) != ending_point:
+            if (x,y) not in random_obstacles_loc:
+                random_obstacles_loc.append((x,y))
+                grid[x][y] = 1
+            else: 
+                continue
+        else:
+            continue
+        current_obstacle += 1
+    print("20 new random obstacles locations: \n", random_obstacles_loc)
+    
+
+random_obstacles(grid)
 print("\n")
-print("> for visual path: \n")
-formatting(path)
-print("\n")
-print("PATH: ", path)
+print("Grid with new obstacles:\n")
+print_grid(grid)
+
+print("\nGrid with new visual path:\n")
+path = find_path(starting_point, ending_point)
+print_grid(grid, path)
+print("\nPATH Phase 2: ", path)
